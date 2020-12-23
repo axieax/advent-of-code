9d983fec0f06f41b24605f2ff4f83b85f05dcefd
Andrew <x.and.i.rew.e@gmail.com>
2020-12-23 13:42:26 +1100

    completed day21

--- day21/p1.py
+++ day21/p1.py
@@ -4,38 +4,30 @@ from functools import reduce
 with open('input.txt', 'r') as f:
     lines = [x.rstrip() for x in f.readlines()]
 
-# map from food to ingredients
-recipe = {}
-# map from ingredients to food
-i_f = {}
-# map from possible allergen to foods
-a_f = {}
-
+# can only deduce that for each allergen, all the missing ingredients are not it
 all_ingredients = set()
-for food, line in enumerate(lines):
+all_allergens = set()
+food_info = []
+ingredient_freq = {}
+for line in lines:
     # extract data
     ingredients, allergens = line[:-1].split(' (contains ')
-    ingredients = ingredients.split(' ')
-    allergens = allergens.split(', ')
-    all_ingredients = all_ingredients.union(set(ingredients))
-    # mapping from possible allergen to foods
-    for allergen in allergens:
-        a_f[allergen] = a_f.get(allergen, []) + [food]
-    # mapping from food to ingredients
-    recipe[food] = set(ingredients)
-    # mapping from ingredient to foods
+    ingredients = set(ingredients.split(' '))
+    allergens = set(allergens.split(', '))
+    # update corresponding data structures
+    all_ingredients |= ingredients
+    all_allergens |= allergens
+    food_info.append((ingredients, allergens))
     for ingredient in ingredients:
-        i_f[ingredient] = i_f.get(ingredient, []) + [food]
+        ingredient_freq[ingredient] = ingredient_freq.get(ingredient, 0) + 1
 
-# examine the ingredients common to all foods with the same allergens
-bad_ingredients = set()
-# for each allergen,
-for possible_allergen, foods in a_f.items():
-    # find the common ingredients between all the foods with the same allergens
-    common = reduce(lambda x, y: x.intersection(y), [recipe[food] for food in foods])
-    # these are bad ingredients as they cause allergens
-    bad_ingredients = bad_ingredients.union(common)
-good_ingredients = all_ingredients - bad_ingredients
+# safe: allergens -> ingredients
+safe = {}
+for ingredients, allergens in food_info:
+    missing_ingredients = all_ingredients - ingredients
+    for allergen in allergens:
+        safe[allergen] = safe.get(allergen, set()) | missing_ingredients
 
-# compute solution
-print(sum([len(i_f[ingredient]) for ingredient in good_ingredients]))
+# the safe ingredients (definitely not it) are safe from all allergens
+safe_ingredients = reduce(lambda x, y: x & y, safe.values())
+print(sum([ingredient_freq[ingredient] for ingredient in safe_ingredients]))
--- day21/p2.py
+++ day21/p2.py
@@ -4,79 +4,51 @@ from functools import reduce
 with open('input.txt', 'r') as f:
     lines = [x.rstrip() for x in f.readlines()]
 
-# map from food to ingredients
-recipe = {}
-# map from ingredients to food
-i_f = {}
-# map from possible allergen to foods
-a_f = {}
-# map from food to possible allergens
-f_a = {}
-
-all_allergens = set()
+# can only deduce that for each allergen, all the missing ingredients are not it
 all_ingredients = set()
-for food, line in enumerate(lines):
+all_allergens = set()
+food_info = []
+for line in lines:
     # extract data
     ingredients, allergens = line[:-1].split(' (contains ')
-    ingredients = ingredients.split(' ')
-    allergens = allergens.split(', ')
-    all_ingredients = all_ingredients.union(set(ingredients))
-    all_allergens = all_allergens.union(set(allergens))
-    # mapping from possible allergen to foods
+    ingredients = set(ingredients.split(' '))
+    allergens = set(allergens.split(', '))
+    # update corresponding data structures
+    all_ingredients |= ingredients
+    all_allergens |= allergens
+    food_info.append((ingredients, allergens))
+
+# safe: allergens -> ingredients
+safe = {}
+for ingredients, allergens in food_info:
+    missing_ingredients = all_ingredients - ingredients
     for allergen in allergens:
-        a_f[allergen] = a_f.get(allergen, []) + [food]
-    # mapping from food to ingredients
-    recipe[food] = set(ingredients)
-    # mapping from ingredient to foods
-    for ingredient in ingredients:
-        i_f[ingredient] = i_f.get(ingredient, []) + [food]
-    # mapping from food to possible allergens
-    f_a[food] = allergens
+        safe[allergen] = safe.get(allergen, set()) | missing_ingredients
 
-# examine the ingredients common to all foods with the same allergens
-bad_ingredients = set()
-# for each allergen,
-for possible_allergen, foods in a_f.items():
-    # find the common ingredients between all the foods with the same allergens
-    common = reduce(lambda x, y: x.intersection(y), [recipe[food] for food in foods])
-    # these are bad ingredients as they cause allergens
-    bad_ingredients = bad_ingredients.union(common)
-good_ingredients = all_ingredients - bad_ingredients
+# the safe ingredients (definitely not it) are safe from all allergens
+safe_ingredients = reduce(lambda x, y: x & y, safe.values())
 
-# compute solution
-# a_i = a_f, f_i
-a_i = {}
-for allergen in all_allergens:
-    foods = a_f[allergen]
-    a_i[allergen] = set([ingredient for ingredient in recipe[food] for food in foods if ingredient in bad_ingredients])
-    print(a_i[allergen])
-    # remove good ingredients
+# possible: allergens -> ingredients
+possible = {}
+for allergen, ingredients in safe.items():
+    # possible ingredients are the ones which are not safe (and not definitely safe)
+    possible[allergen] = all_ingredients - ingredients - safe_ingredients
 
-print(a_i)
-print(good_ingredients)
+# Find unique ingredient for each allergen
+eliminated = set()
+while True:
+    unique_allergens = 0
+    for allergen, ingredients in possible.items():
+        if len(ingredients) == 1:
+            # specify the ingredient as unique
+            eliminated |= ingredients
+            unique_allergens += 1
+        else:
+            # remove eliminated ingredients
+            possible[allergen] -= eliminated
+    # exit when all allergens match to a unique ingredient
+    if unique_allergens == len(all_allergens):
+        break
 
-
-
-# print(bad_ingredients)
-# print(recipe)
-# print(i_f)
-# print(a_f)
-
-
-# # for each ingredient, for each possible allergen
-# # a definite allergen is where the allergen is present for all foods using the ingredient
-# for ingredient, foods in i_f.items():
-#     # ignore good ingredients
-#     if ingredient in good_ingredients:
-#         continue
-#     print(ingredient, foods)
-#     # find possible allergens
-#     possible_allergens = set()
-#     for food in foods:
-#         possible_allergens = possible_allergens.union(set(f_a[food]))
-#     print(possible_allergens)
-#     # for all foods using ingredient, make sure allergen is present
-#     for allergen in possible_allergens:
-#         if all([allergen in f_a[food] for food in foods]):
-#             print(allergen)
-
+# extract single member from set by list(set)[0]
+print(','.join([list(ingredient)[0] for allergen, ingredient in sorted(possible.items(), key=lambda pair: pair[0])]))
