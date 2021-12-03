23d9348909a05616ef5b7d09db52955987a058fc
Andrew <x.and.i.rew.e@gmail.com>
2021-12-03 17:50:51 +1100

    cleaned up day3 solutions

--- 2021/day03/part1.py
+++ 2021/day03/part1.py
@@ -1,32 +1,18 @@
-import math
+from collections import Counter
 
-# with open("sample.txt") as f:
 with open("input.txt") as f:
     data = [x.strip() for x in f.readlines()]
 
-# map each position
 gamma = 0
 epsilon = 0
 for digit_position in range(len(data[0])):
-    bits = [line[digit_position] for line in data]
     # find most common bit
-    num0 = num1 = 0
-    for bit in bits:
-        num0 += bit == "0"
-        num1 += bit == "1"
-    gamma = (gamma << 1) | (num1 > num0)
-    epsilon = (epsilon << 1) | (num0 > num1)
-
-# get significant bit mask
-# most significant bit: ceil(2 ** msb) = gamma
-# msb = math.floor(math.log(gamma, 2))
-# mask = int("1" * (msb + 1), 2)
-
-# epsilon is gamma flipped (bitwise)
-# epsilon = ~gamma & mask
+    bits = [int(line[digit_position]) for line in data]
+    mcb = Counter(bits).most_common(1)[0][0]
+    gamma = (gamma << 1) | mcb
+    epsilon = (epsilon << 1) | (~mcb & 1)
 
 power = gamma * epsilon
-
 print(f"{gamma=}")
 print(f"{epsilon=}")
 print(f"{power=}")
--- 2021/day03/part2.py
+++ 2021/day03/part2.py
@@ -1,54 +1,46 @@
-# with open("sample.txt") as f:
+from collections import Counter
+
 with open("input.txt") as f:
     data = [x.strip() for x in f.readlines()]
+    num_digits = len(data[0])
 
 # calculate oxygen rating
-oxygen_data = data
-num_digits = len(oxygen_data[0])
+oxygen_data = data.copy()
+co2_data = data.copy()
+
+
+def most_common_bit(data: list[str], digit_position: int) -> str:
+    """
+    Finds the most common bit for the given dataset at a given position
+
+    :param data list[str]: dataset
+    :param digit_position int: digit position to be compared
+    :rtype str: most common bit found as a string, returning 1 as the default value
+    """
+    bits = [line[digit_position] for line in data]
+    first, second = Counter(bits).most_common()
+    equal = first[1] == second[1]
+    return first[0] if not equal else "1"
+
+
 for digit_position in range(num_digits):
-    if len(oxygen_data) == 1:
+    # oxygen or co2 dataset exhausted
+    if 1 in (len(oxygen_data), len(co2_data)):
         break
 
-    # find most common bit
-    bits = [line[digit_position] for line in oxygen_data]
-    num0 = num1 = 0
-    for bit in bits:
-        num0 += bit == "0"
-        num1 += bit == "1"
-    # keep 1 if even as well
-    most_common_bit = "1" if num1 >= num0 else "0"
+    # filter oxygen data
+    mcb_oxygen = most_common_bit(oxygen_data, digit_position)
+    oxygen_data = [line for line in oxygen_data if line[digit_position] == mcb_oxygen]
 
-    # filter data
-    oxygen_data = [
-        line for line in oxygen_data if line[digit_position] == most_common_bit
-    ]
+    # filter co2 data (flip most common bit to get least common bit)
+    mcb_co2 = most_common_bit(co2_data, digit_position)
+    lcb_co2 = str((~int(mcb_co2)) & 1)
+    co2_data = [line for line in co2_data if line[digit_position] == lcb_co2]
 
-oxygen_rating = int(oxygen_data[0], 2)
-print(oxygen_rating)
+oxygen = int(oxygen_data[0], 2)
+co2 = int(co2_data[0], 2)
+power = oxygen * co2
 
-
-# calculate co2 rating
-co2_data = data
-num_digits = len(co2_data[0])
-for digit_position in range(num_digits):
-    if len(co2_data) == 1:
-        break
-
-    # find most common bit
-    bits = [line[digit_position] for line in co2_data]
-    num0 = num1 = 0
-    for bit in bits:
-        num0 += bit == "0"
-        num1 += bit == "1"
-    # keep 1 if even as well
-    most_common_bit = "1" if num1 < num0 else "0"
-
-    # filter data
-    co2_data = [line for line in co2_data if line[digit_position] == most_common_bit]
-
-co2_rating = int(co2_data[0], 2)
-print(co2_rating)
-
-power = oxygen_rating * co2_rating
-
+print(f"{oxygen=}")
+print(f"{co2=}")
 print(f"{power=}")
