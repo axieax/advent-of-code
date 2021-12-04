220fe6a54478116b38487152c339685c22445eb6
Andrew <x.and.i.rew.e@gmail.com>
2021-12-04 16:51:27 +1100

    more day4 progress

--- 2021/day04/part1.py
+++ 2021/day04/part1.py
@@ -8,12 +8,19 @@ with open("sample.txt") as f:
 
 boards = data.split("\n\n")
 draws: list[str] = boards.pop(0).split(",")
+boards = "\n\n".join(boards)
+print(boards)
 
 # TODO: compile search regex
+horizontal_pattern = re.compile(" ".join(("-1",) * 5))
+vertical_pattern = re.compile("\n".join(("-1.*?",) * 5))
+diagonal_gaps = (0, 3, 6, 9, 12)
+diagonal_pattern_1 = re.compile("\n".join(f".{gap}-1.*?" for gap in diagonal_gaps))
+diagonal_pattern_2 = re.compile("\n".join(f".*?-1.{gap}" for gap in diagonal_gaps))
 
 for draw in draws:
-    pattern = draw if len(draw) > 1 else " " + draw
-    checker = re.compile(pattern)
+    sub_pattern = draw if len(draw) > 1 else " " + draw
+    checker = re.compile(sub_pattern)
     # TODO: boards together
     for board in boards:
         checker.sub("-1", board)
